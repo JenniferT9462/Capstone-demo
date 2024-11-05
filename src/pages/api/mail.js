e6903030eb66a5622e2e5687e06b861438fdfeb6
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-05 11:48:12 -0600

    Add increment and comments explaining code

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -8,21 +8,32 @@ const redis = Redis.fromEnv();
 
 
 export default async function handler(req, res) {
+    //Set queries for URL - That later will be used for user inputs
     const query = req.query;
     const name = query.name;
     const message = query.message;
     const subject = query.subject;
 
-    await redis.set("item", "poop");
-    const result = await redis.get("item");
+
+    // //Increment a Value
+    await redis.incr('count'); 
     
+    //Set a Value - set("key", value)
+    await redis.set("item", "poop"); 
+    //Get a Value - By calling it's key-"item"
+    const result = await redis.get("item"); 
+    
+    //Email Object
     const email = {
         from: 'Acme <onboarding@resend.dev>',
         to: ['jennifertarleton@gmail.com'],
         subject: 'Hello World',
         html: `<strong>${message}</strong>`,
     }
-    // await resend.emails.send(email);
+    //Initialize email sending w/email object from above
+    //await resend.emails.send(email);
+
+    //Set up ok response
     res.status(200).json({
         name: name, subject: subject, message: message, result: result
     })
