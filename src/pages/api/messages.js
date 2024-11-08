c68440579c2c7e6a7bbc06feadefa76c1e384696
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-08 11:57:03 -0600

    Api route to get stored messages

---
+++ src/pages/api/messages.js
@@ -0,0 +1,18 @@
+// pages/api/messages.js
+import { Redis } from "@upstash/redis";
+
+
+// Initialize Redis connection from environment config
+const redis = Redis.fromEnv();
+
+
+export default async function Handler(req, res){
+    const name = req.query.name
+    const message = await redis.get(`message:${name}`);
+    const email = await redis.get(`email:${name}`);
+    res.status(200).json({
+        message: message,
+        email: email
+        
+    });
+}
