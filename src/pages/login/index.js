3895152c80d02bdf41dff1f0e059541a75126e81
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-14 12:54:55 -0600

    Added a LogIn page

---
+++ src/pages/login/index.js
@@ -0,0 +1,9 @@
+import LogInCard from "@/components/LogInCard";
+
+export default function LoginPage() {
+    return (
+        <div className="min-h-screen overflow-scroll bg-gradient-to-r from-blue-500 to-purple-500">
+             <LogInCard/>
+        </div>
+    );
+}
