e5ddeb4df4c308dc66869625c8ab8894651a3f7c
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-14 23:04:34 -0600

    Made DaysGrid that displays the numbers of the month dynamically

---
+++ src/components/DaysGrid.js
@@ -0,0 +1,26 @@
+
+
+export default function DaysGrid({daysInMonth, firstDayOfMonth, currentDate, currentMonth, currentYear, onDayClick}) {
+    return (
+        <div className="days">
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <span
+              key={day + 1}
+              className={
+                day + 1 === currentDate.getDate() &&
+                currentMonth === currentDate.getMonth() &&
+                currentYear === currentDate.getFullYear()
+                  ? "current-day"
+                  : ""
+              }
+              onClick={() => onDayClick(day + 1)}
+            >
+              {day + 1}
+            </span>
+          ))}
+        </div>
+    );
+}
