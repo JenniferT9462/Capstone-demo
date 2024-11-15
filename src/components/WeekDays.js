f71cd8bf481636a4449aca9d0e88f45c9964618b
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-15 14:51:22 -0600

    Spaced week days out

--- src/components/WeekDays.js
+++ src/components/WeekDays.js
@@ -3,7 +3,7 @@
 export default function WeekDays() {
     const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return (
-        <div>
+        <div className="flex space-x-4 p-2">
         
           {daysOfWeek.map((day) => (
             <span key={day}>{day}</span>
