b9b722333739eee314d0ba5177e3d84fe45ce997
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-16 11:27:16 -0600

    Testing date-fns package

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,12 +1,15 @@
 import CalendarApp from "@/components/CalendarApp";
 
-
+import TestCalendar from "@/components/TestCalendar";
 
 
 export default function CalendarPage() {
     return (
         <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
-            <CalendarApp/>
+            {/* <CalendarApp/> */}
+            <div className="min-h-screen bg-gray-50">
+                <TestCalendar />
+            </div>
         </div>
-    );
+    );  
 }
