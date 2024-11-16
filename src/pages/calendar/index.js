47f1d0890349e3337ae38717be30ba5f81ab49c8
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-16 13:58:55 -0600

    Updated calendar page to have calendar app instead of testCalendar

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -1,14 +1,14 @@
 import CalendarApp from "@/components/CalendarApp";
 
-import TestCalendar from "@/components/TestCalendar";
+
 
 
 export default function CalendarPage() {
     return (
         <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
             {/* <CalendarApp/> */}
-            <div className="min-h-screen bg-gray-50">
-                <TestCalendar />
+            <div className="min-h-screen">
+                <CalendarApp/>
             </div>
         </div>
     );  
