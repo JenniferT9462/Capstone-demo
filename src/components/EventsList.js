a99df3f7056b3170029112e5159af3ea640e46c6
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-16 15:48:42 -0600

    Resize event list

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -3,7 +3,7 @@ import EventCard from "./EventCard";
 
 export default function EventsList({ events, onEdit, onDelete }) {
     return (
-        <div>
+        <div className="w-2/3">
             <h2 className="text-3xl">Events</h2>
             {events.map((event, index) => (
   	        <EventCard key={event.id} event={event} onEdit={onEdit} onDelete={onDelete} />
