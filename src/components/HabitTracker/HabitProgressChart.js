abaace08e825f972374c34b64ba0a2e3727069a7
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-16 18:48:41 -0600

    Add plugins to include legend for pie chart

--- src/components/HabitTracker/HabitProgressChart.js
+++ src/components/HabitTracker/HabitProgressChart.js
@@ -1,19 +1,34 @@
+
 import { Pie } from "react-chartjs-2";
 
-export default function HabitProgressChart({ habits }) {
-    const data = {
-        labels: habits.map(habit => habit.name),
-        datasets: [
-            {
-                data: habits.map(habit => habit.progress),
-                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
-                hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
 
-     }, ],
-    };
-    return (
-        <div className="w-full max-w-md mx-auto mt-4">
-            <Pie data={data}/>
-        </div>
-    );
-}
+export default function HabitProgressChart({ habits }) {
+    
+        const data = {
+          labels: habits.map(habit => habit.name),
+          datasets: [
+            {
+              data: habits.map(habit => habit.progress),
+              backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
+              hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'],
+            },
+          ],
+        };
+        const options = {
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top',
+                },
+                tooltip: {
+                    enabled: true,
+                },
+            },
+        };
+        return (
+          <div className="w-full max-w-md mx-auto mt-4 p-4">
+            <Pie data={data} options={options}/>
+          </div>
+        );
+      };
+      
