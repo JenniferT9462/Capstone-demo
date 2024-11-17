ad601c53d992a704a426480af17a4d235c32c453
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-17 12:28:48 -0600

    Positioning elements

--- src/pages/journal/index.js
+++ src/pages/journal/index.js
@@ -1,19 +1,22 @@
 import Journal from "@/components/Journal";
 import MoodCalendar from "@/components/MoodCalendar";
+import MoodChart from "@/components/MoodChart";
 import MoodTracker from "@/components/MoodTracker";
 
 export default function JournalPage() {
     return (
         <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
            <h1 className="py-4 text-center text-4xl text-white">Journal Page</h1>
-           <div>
-                <h1 className="text-2xl ml-4 text-white">Calendar</h1>
+           <h1 className="text-2xl text-white">Calendar</h1>
+           <div className="flex mt-10">
                 <MoodCalendar/>
+                <div className="bg-white p-4 rounded-md w-1/2 shadow-md float-right mr-12 ">
+                    <h2 className="text-blue-500">Moods</h2>
+                    <MoodTracker/>
+                </div>
+               
             </div>
-            <div className="bg-white p-4 rounded-md w-1/2 shadow-md  mt-0 mr-12">
-                <h2 className="text-blue-500">Moods</h2>
-                <MoodTracker/>
-            </div>
+            
             <div>
                 <Journal/>
             </div>
