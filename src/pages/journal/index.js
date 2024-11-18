ba17daa0b0e37ba20efbafcd1655ba780b6e72ae
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-18 17:08:44 -0600

    Deleted test apis

--- src/pages/journal/index.js
+++ src/pages/journal/index.js
@@ -4,10 +4,11 @@ import MoodTracker from "@/components/JournalPage/MoodTracker";
 import Navbar from "@/components/Main/NavBar";
 
 export default function JournalPage() {
-    return (
+   return (
         <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-500">
             <Navbar/>
            <h1 className="py-4 text-center text-4xl text-white">Journal Page</h1>
+           
            <div>
                 <MoodCalendar/>
                 <MoodTracker/>
--- src/pages/login/index.js
+++
@@ -1,9 +0,0 @@
-import LogInCard from "@/components/Main/LogInCard";
-
-export default function LoginPage() {
-    return (
-        <div className="min-h-screen overflow-scroll bg-gradient-to-r from-blue-500 to-purple-500">
-             <LogInCard/>
-        </div>
-    );
-}
