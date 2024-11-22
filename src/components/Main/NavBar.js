422366eb47e2dfc4cc1d778f0b0d0862d3d9e1d2
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-22 10:55:51 -0600

    Changed app name and added link to about page

--- src/components/Main/NavBar.js
+++ src/components/Main/NavBar.js
@@ -14,12 +14,12 @@ export default function Navbar() {
           <Logo/>
         </div>
         <h1 className="text-xl text-white font-bold">
-          Planner App
+          ZenFlow
         </h1>
       </div>
       {/* Nav links */}
       <div className="flex space-x-4 md:space-x-2">
-          <Link href="/" className="text-white text-xl md:text-sm hover:text-blue-300">
+          <Link href="/welcome" className="text-white text-xl md:text-sm hover:text-blue-300">
             Home
           </Link>
           <Link href="/calendar" className="text-white text-xl md:text-sm hover:text-blue-300">
@@ -30,6 +30,9 @@ export default function Navbar() {
           </Link>
           <Link href="/journal" className="text-white text-xl md:text-sm hover:text-blue-300">
             Journal
+          </Link>
+          <Link href="/about" className="text-white text-xl md:text-sm hover:text-blue-300">
+            About
           </Link>
       </div>
     </nav>
