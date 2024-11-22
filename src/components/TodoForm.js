3cbab785d2a90d147d799a90f48d68a718640e33
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-22 09:52:27 -0600

    Saving todos to redis from api

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,17 +3,30 @@ import { useState } from "react";
 export default function TodoForm({ onAddTodo}) {
     const [todoTitle, setTodoTitle] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (todoTitle.trim()) { //Removes the leading and trailing white space and line terminator characters from a string.
-            onAddTodo({
+           const newTodo = {
                 id: Date.now(), //Unique Id for the todo
                 title: todoTitle,
                 completed: false,
-            })
-            setTodoTitle("") // Clear the input field
+            };
+             // Call the onAddTodo function to handle API interaction
+             try {
+                const response = await fetch('/api/saveTodos', {
+                    method: 'POST',
+                    headers: { "Content-Type": "application/json"},
+                    body: JSON.stringify(newTodo)
+                  });
+                if (response.ok) {
+                    await onAddTodo(newTodo);
+                    setTodoTitle("") // Clear the input field
+                }
+            } catch (error) {
+                console.error("Failed to add todo:", error)
+             }
         }
-    }
+    };
     return (
         <div className="w-1/3 bg-white rounded-md shadow-md">
             <form onSubmit={handleSubmit} className="mt-4">
